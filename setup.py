10f6fb3977bb5d134263245a70827ffe08011c85
jojossev <jojossev@users.noreply.github.com>
2025-08-05 21:16:09 +0100

    Fix: Mise à jour de setup.py pour résoudre l'erreur de build

--- setup.py
+++ setup.py
@@ -1,20 +1,12 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name="uniapp",
-    version="0.1",
+    version="0.1.0",
     packages=find_packages(),
-    install_requires=[
-        "Django==4.2.10",
-        "psycopg2-binary==2.9.9",
-        "Pillow==9.5.0",
-        "gunicorn==21.2.0",
-        "whitenoise==6.6.0",
-        "dj-database-url==2.1.0",
-        "python-decouple==3.8",
-        "python-dotenv==1.0.0",
-        "crispy-bootstrap5==2024.2",
-        "django-crispy-forms==2.1",
-        "django-allauth==0.61.1"
-    ],
+    install_requires=requirements,
+    python_requires='>=3.10',
 )
