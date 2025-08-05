cfb0f1afdc6669c345cdcd347f1e98c0488693b6
jojossev <jojossev@users.noreply.github.com>
2025-08-05 19:27:10 +0100

    Fix: Mise à jour de la configuration de la base de données pour PostgreSQL

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -109,11 +109,15 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import dj_database_url
+
+# Configuration pour PostgreSQL avec dj-database-url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 
